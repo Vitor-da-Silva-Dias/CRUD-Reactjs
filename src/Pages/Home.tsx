ae5e12113b6f3d24bcf1a7cded008315604e91ba
Vitor da Silva Dias <diasdasilvavitor@gmail.com>
2023-04-18 22:39:38 -0300

    criado-modal-cadastro

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import Login from "../components/Login";
 import { Grid } from "@mui/material";
 
+
 const Home: React.FC = () => {
     return(
         <Grid container height={'100vh'} justifyContent={'center'} alignItems={'center'}>
-            <Grid item xs={2}>
+            <Grid item xs={4}>
                 <Login/>
             </Grid>
         </Grid>
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,9 +1,26 @@
-import { Button, TextField, Typography } from '@mui/material';
+import { Box, Button, Modal, TextField, Typography } from '@mui/material';
 import React, { useState } from 'react';
+
 
 const Login: React.FC = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -12,19 +29,42 @@ const Login: React.FC = () => {
   };
 
   return (
+    <>
     <div>
       <Typography variant='h4' textAlign={'center'}>Login</Typography>
       <br/><br/>
       <form onSubmit={handleSubmit}>
-          <TextField placeholder='e-mail' type="text" value={username} onChange={(event) => setUsername(event.target.value)} />
+          <TextField fullWidth placeholder='e-mail' type="text" value={username} onChange={(event) => setUsername(event.target.value)} />
         <br/><br/>
-          <TextField placeholder='password' type="password" value={password} onChange={(event) => setPassword(event.target.value)} />
+          <TextField fullWidth placeholder='password' type="password" value={password} onChange={(event) => setPassword(event.target.value)} />
         <br/><br/>
-        <Button variant='contained' fullWidth type="submit">Login</Button>
+        <Button variant='contained' fullWidth type="submit">Entrar</Button>
         <br/><br/>
-        <Button variant='contained' color='success' fullWidth type="submit">Sign Up</Button>
+        <Button variant='contained' color='success' fullWidth onClick={handleOpen}>Criar conta</Button>
       </form>
     </div>
+    <div>
+    
+    <Modal
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>
+        <Typography variant='h4' textAlign={'center'}>Criar uma nova conta</Typography>
+        <br/><br/>
+        <TextField fullWidth placeholder='e-mail' type="text" value={username} onChange={(event) => setUsername(event.target.value)} />
+        <br/><br/>
+        <TextField fullWidth placeholder='password' type="password" value={username} onChange={(event) => setUsername(event.target.value)} />
+        <br/><br/>
+        <TextField fullWidth placeholder='confirm password' type="password" value={username} onChange={(event) => setUsername(event.target.value)} />
+        <br/><br/>
+        <Button variant='contained' color='success' fullWidth>Criar conta</Button>
+      </Box>
+    </Modal>
+  </div>
+  </>
   );
 };
 
