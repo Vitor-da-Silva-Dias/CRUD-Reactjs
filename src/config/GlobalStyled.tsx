82504234d621c60732d4cb747147edae9e0730a3
Vitor da Silva Dias <diasdasilvavitor@gmail.com>
2023-05-08 22:43:15 -0300

    margens-desnecessarias-removidas

--- src/config/GlobalStyled.tsx
+++ src/config/GlobalStyled.tsx
@@ -2,6 +2,10 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyled = createGlobalStyle`
     *, html, body {
+        margin-top: 0;
+        margin-bottom: 0;
+        padding-top: 0;
+        padding-bottom: 0;
         font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
             'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
             sans-serif;
