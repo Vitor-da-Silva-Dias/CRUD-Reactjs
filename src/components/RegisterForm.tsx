13aa9be4f63211dfde959cbdf732c6841a131eb6
Vitor-da-Silva-Dias <diasdasilvavitor@gmail.com>
2023-06-21 20:42:29 -0300

    integração da página de cadastro com o back end

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -1,9 +1,8 @@
 import { Button, TextField, Typography } from '@mui/material';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import User from '../types/User';
-
-
 
 const Register: React.FC = () => {
   const [email, setEmail] = useState('');
@@ -11,19 +10,11 @@ const Register: React.FC = () => {
   const [passwordConfirm, setPasswordConfirm] = useState('');
   const navigate = useNavigate();
 
-  const allUsers: User[] = JSON.parse(localStorage.getItem('allUsers') ?? '[]');
-
-  const handleSignUp = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSignUp = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     if (email.length < 6) {
       alert('Preencha o campo com um e-mail válido.');
-      return;
-    }
-
-    const checkUser = allUsers.find((user) => user.email === email);
-    if (checkUser) {
-      alert('Email já cadastrado');
       return;
     }
 
@@ -37,40 +28,67 @@ const Register: React.FC = () => {
       return;
     }
 
-    const newUser: User = {
-      email,
-      password,
-      errands: [],
-    };
+    try {
+      const newUser: User = {
+        email,
+        password,
+        errands: [],
+      };
 
-    allUsers.push(newUser);
+      const response = await axios.post(`http://localhost:3333/users`, newUser);
 
-    alert('Conta criada com sucesso.');
-
-    saveAccount(allUsers);
-
-    navigate ('/');
-  };
-
-  const saveAccount = (users: User[]) => {
-    localStorage.setItem('allUsers', JSON.stringify(users));
+      if (response.status === 201) {
+        alert('Conta criada com sucesso.');
+        navigate('/');
+      } else {
+        alert('Falha ao criar a conta. Por favor, tente novamente.');
+      }
+    } catch (error) {
+      console.error('Erro ao criar a conta:', error);
+      alert('Ocorreu um erro ao criar a conta. Por favor, tente novamente.');
+    }
   };
 
   return (
     <div>
-    <Typography variant='h4' textAlign={'center'}>Sign Up</Typography>
-      <br/><br/>
-    <form onSubmit={handleSignUp}>
-        <TextField fullWidth label='email' type='email' value={email} onChange={(e) => setEmail(e.target.value)}/>
-        <br/><br/>
-        <TextField fullWidth label='password' type='password' value={password} onChange={(e) => setPassword(e.target.value)}/>
-        <br/><br/>
-        <TextField fullWidth label='confirm password' type='password' value={passwordConfirm} onChange={(e) => setPasswordConfirm(e.target.value)}/>
-        <br/><br/>
-        <Button type='submit' variant='contained' color='success' fullWidth>Create Account</Button>
-    </form>
+      <Typography variant="h4" textAlign={'center'}>
+        Sign Up
+      </Typography>
+      <br />
+      <br />
+      <form onSubmit={handleSignUp}>
+        <TextField
+          fullWidth
+          label="email"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br />
+        <br />
+        <TextField
+          fullWidth
+          label="password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
+        <br />
+        <TextField
+          fullWidth
+          label="confirm password"
+          type="password"
+          value={passwordConfirm}
+          onChange={(e) => setPasswordConfirm(e.target.value)}
+        />
+        <br />
+        <br />
+        <Button type="submit" variant="contained" color="success" fullWidth>
+          Create Account
+        </Button>
+      </form>
     </div>
-    
   );
 };
 
