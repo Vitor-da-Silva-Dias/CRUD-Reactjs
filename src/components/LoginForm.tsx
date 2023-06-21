5ad3968271bcf7f6f9fd0029b16bedd7a74b2ca6
Vitor-da-Silva-Dias <diasdasilvavitor@gmail.com>
2023-06-21 20:58:54 -0300

    integração da página de login com o back end

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,57 +1,79 @@
 import React, { useState } from 'react';
 import { Button, TextField, Typography } from '@mui/material';
 import { useNavigate, Link } from 'react-router-dom';
-import User from '../types/User';
-
- 
+import axios from 'axios';
 
 const Login: React.FC = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
 
-  const allUsers: User[] = JSON.parse(localStorage.getItem('allUsers') ?? '[]');
-
-  function handleLogin(e: React.FormEvent<HTMLFormElement>) {
+  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    const checkUser = allUsers.find(
-      (user) =>
-        user.email === email && user.password === password
-    );
+    try {
+      const response = await axios.post('http://localhost:3333/users/login', {
+        email,
+        password,
+      });
 
+      const { ok, message, data } = response.data;
 
-    if (!checkUser) {
-      alert('Email ou senha incorreta');
-      return;
+      if (ok) {
+        alert('Login realizado com sucesso.');
+
+        sessionStorage.setItem('token', data.token);
+
+        navigate('/recados');
+      } else {
+        alert(message);
+      }
+    } catch (error) {
+      console.log(error);
+      alert('Ocorreu um erro ao fazer login.');
     }
-
-    alert('Login realizado com sucesso.');
-
-    sessionStorage.setItem('logged', JSON.stringify(checkUser));
-
-    navigate ('/recados');
-  }
-
+  };
 
   return (
     <>
-    <div>
-      <Typography variant='h4' textAlign={'center'}>Login</Typography>
-      <br/><br/>
-      <form onSubmit={handleLogin}>
-          <TextField fullWidth label='e-mail' type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-        <br/><br/>
-          <TextField fullWidth label='password' type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-        <br/><br/>
-          <Button variant='contained' fullWidth type="submit">Entrar</Button>
-        <br/><br/>
-        <Link to = "/signup" style={{textDecoration:'none'}}>
-        <Button variant='contained' color='success' fullWidth>Criar conta</Button>
-        </Link>
-      </form>
-    </div>
-  </>
+      <div>
+        <Typography variant="h4" textAlign="center">
+          Login
+        </Typography>
+        <br />
+        <br />
+        <form onSubmit={handleLogin}>
+          <TextField
+            fullWidth
+            label="E-mail"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <br />
+          <br />
+          <TextField
+            fullWidth
+            label="Senha"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <br />
+          <br />
+          <Button variant="contained" fullWidth type="submit">
+            Entrar
+          </Button>
+          <br />
+          <br />
+          <Link to="/signup" style={{ textDecoration: 'none' }}>
+            <Button variant="contained" color="success" fullWidth>
+              Criar conta
+            </Button>
+          </Link>
+        </form>
+      </div>
+    </>
   );
 };
 
